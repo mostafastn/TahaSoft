f694eef0aa9071efb171d3d5fcdec6a34b2a2615
Mostafa <Mostafa_stn@yahoo.com>
2018-09-25 23:36:29 +0330

    category and customer edited

--- Taha.Domains/Customer.cs
+++ Taha.Domains/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,11 @@ namespace Taha.Domains
         {
             Orders = new List<Order>();
         }
+
+        [Required]
         public Guid PersonId { get; set; }
+
+        public virtual Person Person { get; set; }
         public virtual IList<Order> Orders { get; set; }
         public virtual ShippingAddress ShippingAddress { get; set; }
 
