11f349f96b90f01aea835e9d7e08ea7b25ebe1d8
Mostafa <Mostafa_stn@yahoo.com>
2018-09-29 01:07:24 +0330

    Delete Unnecessary Classes

--- Taha.Repository/CategoryRepository.cs
+++ Taha.Repository/CategoryRepository.cs
@@ -1,80 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using Taha.Core.Repository;
 using Taha.DatabaseInitilization;
 using Taha.Domains;
-using Taha.Framework.Repository;
 
 namespace Taha.Repository
 {
-    public class CategoryRepository : IRepository<Category>
+    public class CategoryRepository : BaseRepository<TahaDatabaseContext, Category>
     {
-        public RepositoryResult<IEnumerable<Category>> GetAll(Expression<Func<Category, bool>> filter = null,
-            Func<IQueryable<Category>, IOrderedQueryable<Category>> orderBy = null,
-            params Expression<Func<Category, object>>[] np)
-        {
-            var resylt = new RepositoryResult<IEnumerable<Category>>()
-            {
-                Result = null,
-                Message = "",
-                succeed = false
-            };
-            try
-            {
-                using (var context = new TahaDatabaseContext())
-                {
-                    var query = context.Categories.AsQueryable();
-                    if (filter != null)
-                    {
-                        query = query.Where(filter);
-                    }
-                    if (orderBy != null)
-                    {
-                        query = orderBy(query);
-                    }
-
-                    resylt.Result = query.ToList();
-                }
-                resylt.succeed = true;
-
-            }
-            catch (Exception ex)
-            {
-                resylt.Message = ex.Message;
-            }
-
-            return resylt;
-        }
-
-        public RepositoryResult<Category> Delete(List<Guid> ID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Category> GetByID(Guid ID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Category> GetSingel(Expression<Func<Category, bool>> where, params Expression<Func<Category, object>>[] np)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Category> Insert(List<Category> value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Category> Save()
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Category> Update(List<Category> value)
-        {
-            throw new NotImplementedException();
-        }
+     
     }
 }
--- Taha.Repository/ProductRepository.cs
+++ Taha.Repository/ProductRepository.cs
@@ -2,79 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Taha.Core.Repository;
 using Taha.DatabaseInitilization;
 using Taha.Domains;
 using Taha.Framework.Repository;
 
 namespace Taha.Repository
 {
-    class ProductRepository : IRepository<Product>
+    class ProductRepository : BaseRepository<TahaDatabaseContext, Product>
     {
-        public RepositoryResult<IEnumerable<Product>> GetAll(Expression<Func<Product, bool>> filter = null, 
-            Func<IQueryable<Product>, IOrderedQueryable<Product>> orderBy = null, 
-            params Expression<Func<Product, object>>[] np)
-        {
-            var resylt = new RepositoryResult<IEnumerable<Product>>()
-            {
-                Result = null,
-                Message = "",
-                succeed = false
-            };
-            try
-            {
-                using (var context = new TahaDatabaseContext())
-                {
-                    var query = context.Products.AsQueryable();
-                    if (filter != null)
-                    {
-                        query = query.Where(filter);
-                    }
-                    if (orderBy != null)
-                    {
-                        query = orderBy(query);
-                    }
 
-                    resylt.Result = query.ToList();
-                }
-
-
-            }
-            catch (Exception ex)
-            {
-                resylt.Message = ex.Message;
-            }
-
-            return resylt;
-        }
-
-        public RepositoryResult<Product> Delete(List<Guid> ID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Product> GetByID(Guid ID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Product> GetSingel(Expression<Func<Product, bool>> where, params Expression<Func<Product, object>>[] np)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Product> Insert(List<Product> value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Product> Save()
-        {
-            throw new NotImplementedException();
-        }
-
-        public RepositoryResult<Product> Update(List<Product> value)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
